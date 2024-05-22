78f4e6317359ccf64804479dfd944e5665bffe77
yamotech <nothings.2c9@gmail.com>
2024-05-22 20:34:21 +0900

    :bug: Remove DEPRECATION WARNING: TestFixtures.fixture_path= is deprecated and will be removed in Rails 7.2. Use .fixture_paths= instead.
    DEPRECATION WARNING: TestFixtures.fixture_path= is deprecated and will be removed in Rails 7.2. Use .fixture_paths= instead. (called from <top (required)> at /Users/tomoki.yamauchi/workspace/yamotech/tmp/activerecord-dbt/test/test_helper.rb:10)
    DEPRECATION WARNING: TestFixtures.fixture_path is deprecated and will be removed in Rails 7.2. Use .fixture_paths instead.
    If multiple fixture paths have been configured with .fixture_paths, then .fixture_path will just return
    the first path.
     (called from <top (required)> at /Users/tomoki.yamauchi/workspace/yamotech/tmp/activerecord-dbt/test/test_helper.rb:11)
    DEPRECATION WARNING: TestFixtures.fixture_path= is deprecated and will be removed in Rails 7.2. Use .fixture_paths= instead. (called from <top (required)> at /Users/tomoki.yamauchi/workspace/yamotech/tmp/activerecord-dbt/test/test_helper.rb:11)
    DEPRECATION WARNING: TestFixtures.fixture_path is deprecated and will be removed in Rails 7.2. Use .fixture_paths instead.
    If multiple fixture paths have been configured with .fixture_paths, then .fixture_path will just return
    the first path.
     (called from <top (required)> at /Users/tomoki.yamauchi/workspace/yamotech/tmp/activerecord-dbt/test/test_helper.rb:12)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,8 +7,10 @@ require "rails/test_help"
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
+  ActiveSupport::TestCase.fixture_paths = [File.expand_path("fixtures", __dir__)]
+  ActionDispatch::IntegrationTest.fixture_paths = ActiveSupport::TestCase.fixture_paths
+  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_paths.map do |fixture_path|
+    "#{fixture_path}/files"
+  end
   ActiveSupport::TestCase.fixtures :all
 end
