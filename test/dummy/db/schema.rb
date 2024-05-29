d8ef0f549b82e2732c916b6b60420895292d08e1
yamotech <nothings.2c9@gmail.com>
2024-05-29 21:39:51 +0900

    :sparkles: Execute bin/rails generate model posts user:references title:string content:text & bin/rails db:migrate

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_123202) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_123628) do
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
