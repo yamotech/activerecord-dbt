2aedc2df3b608a95a12c1bdc3292c52acc7b7002
yamotech <nothings.2c9@gmail.com>
2024-05-31 21:08:35 +0900

    :sparkles: Update ConfigGenerator

--- lib/generators/dbt/config/config_generator.rb
+++ lib/generators/dbt/config/config_generator.rb
@@ -3,14 +3,6 @@ module Dbt
     class ConfigGenerator < Rails::Generators::Base
       source_root File.expand_path("templates", __dir__)
 
-      def create_source_header_file
-        template "source_header.yml.tt", "lib/dbt/source_header.yml", source_name
-      end
-
-      def create_source_header_markdown_file
-        template "source_header.md.tt", "lib/dbt/source_header.md", source_name
-      end
-
       def copy_source_description_file
         copy_file "descriptions.yml", "lib/dbt/descriptions.yml"
       end
