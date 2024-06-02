76e8d5264dffc0b949cb749be0bbe2898ca514b3
yamotech <nothings.2c9@gmail.com>
2024-06-02 14:47:01 +0900

    :construction: WIP Parser#convert_hash

--- lib/active_record/dbt/parser.rb
+++ lib/active_record/dbt/parser.rb
@@ -5,7 +5,25 @@ module ActiveRecord
         return {} if path.nil?
 
         yaml_hash = YAML.load_file(path) || {}
-        yaml_hash.with_indifferent_access
+        convert_hash(yaml_hash.with_indifferent_access)
+      end
+
+      private
+
+      def convert_hash(hash)
+        hash.each_with_object({}) do |(key, value), result|
+          result[key] = convert_value(value)
+        end
+      end
+
+      def convert_value(value)
+        if value.is_a?(Hash)
+          convert_hash(value)
+        elsif value.is_a?(String) && value.match?(/\R/)
+          value.split(/\R/).map(&:strip)
+        else
+          value
+        end
       end
     end
   end
