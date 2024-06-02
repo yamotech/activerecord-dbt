c213cff7269c151831c0f3c4b7aa3e9f9525f4a1
yamotech <nothings.2c9@gmail.com>
2024-06-02 18:26:06 +0900

    :sparkles: Add status column to post
    Execute
    - bin/rails generate migration AddStatusColumnToPOST status:integer
    - bin/rails db:migrate
    - Set the enum of posts.status.

--- test/dummy/app/models/post.rb
+++ test/dummy/app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  enum status: %i[draft published deleted]
 end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_123628) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_02_092442) do
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
