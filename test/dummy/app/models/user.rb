6730f87db09739d0796bb361213a439f1860f458
yamotech <nothings.2c9@gmail.com>
2024-06-06 21:29:45 +0900

    :sparkles: Update Relationship model

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   has_many :posts
+
+  has_many :active_relationships,
+           class_name: :Relationship,
+           foreign_key: :follower_id,
+           dependent: :destroy
+  has_many :passive_relationships,
+           class_name: :Relationship,
+           foreign_key: :followed_id,
+           dependent: :destroy
 end
