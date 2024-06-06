29066a0cf4e28cb760f22a8522032276188a66a6
yamotech <nothings.2c9@gmail.com>
2024-06-06 21:25:51 +0900

    :sparkles: Execute bin/rails db:migrate

--- test/dummy/db/migrate/20240606121957_create_relationships.rb
+++ test/dummy/db/migrate/20240606121957_create_relationships.rb
@@ -1,10 +1,12 @@
 class CreateRelationships < ActiveRecord::Migration[7.1]
   def change
     create_table :relationships do |t|
-      t.integer :follower_id
-      t.integer :followed_id
+      t.references :follower, null: false, foreign_key: { to_table: :users }
+      t.references :followed, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
+
+    add_index :relationships, %i[follower_id followed_id], unique: true
   end
 end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_02_092442) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_06_121957) do
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title"
@@ -21,10 +21,22 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_02_092442) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   add_foreign_key "posts", "users"
+  add_foreign_key "relationships", "users", column: "followed_id"
+  add_foreign_key "relationships", "users", column: "follower_id"
 end
