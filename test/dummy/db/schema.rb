61a20f6561ca0036347bb590e35bfd78288b08ce
yamotech <nothings.2c9@gmail.com>
2024-06-09 23:48:41 +0900

    :card_file_box: Execute bin/rails generate model tag name:string & bin/rails db:migrate

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_09_141238) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_09_144327) do
   create_table "companies", force: :cascade do |t|
     t.string "name", null: false
     t.string "establishment_date"
@@ -47,6 +47,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_09_141238) do
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
     t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
