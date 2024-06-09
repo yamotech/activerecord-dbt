d9bb03e2bcb08acd080fc85851c279024f9fc9a5
yamotech <nothings.2c9@gmail.com>
2024-06-09 22:48:58 +0900

    :card_file_box: Execute bin/rails db:migrate

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_06_121957) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_09_121227) do
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "establishment_date"
+    t.float "average_age"
+    t.boolean "published", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title"
@@ -34,9 +43,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_06_121957) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "company_id"
+    t.index ["company_id"], name: "index_users_on_company_id"
   end
 
   add_foreign_key "posts", "users"
   add_foreign_key "relationships", "users", column: "followed_id"
   add_foreign_key "relationships", "users", column: "follower_id"
+  add_foreign_key "users", "companies"
 end
