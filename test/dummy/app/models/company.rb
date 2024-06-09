3917353ce680d0eab515d702f97bb8ea62e35420
yamotech <nothings.2c9@gmail.com>
2024-06-09 22:48:32 +0900

    :card_file_box: Execute bin/rails generate migration AddCompanyIdColumnToUsers company:references

--- test/dummy/app/models/company.rb
+++ test/dummy/app/models/company.rb
@@ -1,4 +1,6 @@
 class Company < ApplicationRecord
+  has_many :users
+
   validates :name, presence: true
   validates :published, presence: true, inclusion: { in: [true, false] }
 end
--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  belongs_to :company, optional: true
   has_many :posts
 
   has_many :active_relationships,
