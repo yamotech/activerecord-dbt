f9ded87bf876fa5bb52c69361ee98b5b72d6fb7e
yamotech <nothings.2c9@gmail.com>
2024-06-30 20:30:20 +0900

    :bug: Require 'active_record/dbt'

--- lib/generators/active_record/dbt/initializer/templates/dbt.rb
+++ lib/generators/active_record/dbt/initializer/templates/dbt.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'active_record/dbt'
+
 ActiveRecord::Dbt.configure do |c|
   c.source_config_path = 'lib/dbt/source_config.yml'
   c.export_directory_path = 'doc/dbt'
