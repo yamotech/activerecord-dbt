97b9365ea0f60d3f79d0b8ef004d87fc80f6f8ff
yamotech <nothings.2c9@gmail.com>
2024-08-14 16:16:26 +0900

    :sparkles: Add ActiveRecord::Dbt::Configuration::I18nConfiguration

--- lib/active_record/dbt/config.rb
+++ lib/active_record/dbt/config.rb
@@ -9,14 +9,10 @@ module ActiveRecord
 
       include ActiveRecord::Dbt::Configuration::DataSync
       include ActiveRecord::Dbt::Configuration::DwhPlatform
+      include ActiveRecord::Dbt::Configuration::I18nConfiguration
       include ActiveRecord::Dbt::Configuration::Logger
       include ActiveRecord::Dbt::Configuration::Source
       include ActiveRecord::Dbt::Configuration::UsedDbtPackage
-
-      def locale=(locale = I18n.locale)
-        I18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
-        I18n.locale = locale
-      end
     end
   end
 end
