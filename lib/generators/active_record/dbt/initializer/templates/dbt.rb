ce95f9eb2ba8e5256eff328eccb35482ee4a3bc6
yamotech <nothings.2c9@gmail.com>
2024-08-20 21:02:10 +0900

    :sparkles: Changed to run in development environment only

--- lib/generators/active_record/dbt/initializer/templates/dbt.rb
+++ lib/generators/active_record/dbt/initializer/templates/dbt.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-require 'active_record/dbt'
+if Rails.env.development?
+  require 'active_record/dbt'
 
-ActiveRecord::Dbt.configure do |c|
-  c.config_directory_path = 'lib/dbt'
-  c.export_directory_path = 'doc/dbt'
-  c.dwh_platform = 'bigquery'
-  c.data_sync_delayed = false
-  c.used_dbt_package_names = []
+  ActiveRecord::Dbt.configure do |c|
+    c.config_directory_path = 'lib/dbt'
+    c.export_directory_path = 'doc/dbt'
+    c.dwh_platform = 'bigquery'
+    c.data_sync_delayed = false
+    c.used_dbt_package_names = []
+  end
 end
