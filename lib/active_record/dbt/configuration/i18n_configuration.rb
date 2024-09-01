1067826dc54212be6b730263f7d3045ab477a1d2
yamotech <nothings.2c9@gmail.com>
2024-09-01 17:22:51 +0900

    :rotating_light: Fix [Correctable] Rails/RootPathnameMethods: Rails.root is a Pathname so you can just append #[].

--- lib/active_record/dbt/configuration/i18n_configuration.rb
+++ lib/active_record/dbt/configuration/i18n_configuration.rb
@@ -5,7 +5,7 @@ module ActiveRecord
     module Configuration
       module I18nConfiguration
         def locale=(locale = I18n.locale)
-          I18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
+          I18n.load_path += Rails.root.glob('config/locales/**/*.{rb,yml}')
           I18n.locale = locale
         end
       end
