5348f791b1d82a5228741d52fd155cdb326f8bb2
yamotech <nothings.2c9@gmail.com>
2024-09-01 15:47:50 +0900

    :truck: Rename ActiveRecord::Dbt::Factory::TablesFactory to ActiveRecord::Dbt::Factory::Tables::YmlFactory

--- lib/active_record/dbt/factory/tables/yml_factory.rb
+++ lib/active_record/dbt/factory/tables/yml_factory.rb
@@ -3,12 +3,12 @@
 module ActiveRecord
   module Dbt
     module Factory
-      module Source
+      module Tables
         module YmlFactory
           def self.build
-            tables_factory = ActiveRecord::Dbt::Factory::Tables::YmlFactory.build
-
-            ActiveRecord::Dbt::Source::Yml.new(tables_factory)
+            ActiveRecord::Base.connection.tables.sort.map do |table_name|
+              ActiveRecord::Dbt::Factory::Table::YmlFactory.build(table_name)
+            end
           end
         end
       end
