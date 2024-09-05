4195d59ab17454a5fad6baa0dffb0c4f155d5ff2
yamotech <nothings.2c9@gmail.com>
2024-09-05 10:36:16 +0900

    :bug: Update ActiveRecord::Dbt::Column::DataTestable::RelationshipsDataTestable#relationships_test

--- lib/active_record/dbt/column/data_testable/relationships_data_testable.rb
+++ lib/active_record/dbt/column/data_testable/relationships_data_testable.rb
@@ -11,11 +11,11 @@ module ActiveRecord
 
           define_required_methods :@config, :foreign_keys, :column_name
 
-          delegate :source_name, :data_sync_delayed?, to: :@config
+          delegate :source_name, :data_sync_delayed?, :exclude_table_names, to: :@config
           delegate :to_table, to: :foreign_key
 
           def relationships_test
-            return nil if foreign_key.blank?
+            return nil if foreign_key.blank? || exclude_table_names.include?(to_table)
 
             {
               'relationships' => {
