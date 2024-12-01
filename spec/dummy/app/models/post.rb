12d8668c8541b5f974f544f5aec58779825b31d4
yamotech <nothings.2c9@gmail.com>
2024-12-02 00:29:48 +0900

    :bug: Fix the way the status enum is specified in Post
    https://github.com/yamotech/activerecord-dbt/actions/runs/12106702814/job/33752746129?pr=31
    
    ```rb
    An error occurred while loading ./spec/active_record/dbt/column/data_testable/not_null_data_testable_spec.rb.
    Failure/Error: enum status: %i[draft published deleted]
    
    ArgumentError:
      wrong number of arguments (given 0, expected 1..2)
    ```

--- spec/dummy/app/models/post.rb
+++ spec/dummy/app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :tags
 
-  enum status: %i[draft published deleted]
+  enum :status, { draft: 0, published: 1, deleted: 2 }
 end
